8174b49f4f696131c59f624f25761068cb339c53
Bruno Chester Leardine <35078348+BrunoChester@users.noreply.github.com>
2020-01-11 20:52:28 -0500

    Update Foresee_data_SCS_study.R

--- Foresee_data_SCS_study.R
+++ Foresee_data_SCS_study.R
@@ -1,8 +1,11 @@
 library(readr)
 library(tidyverse)
-library(stringr)
 
-Foresee <- read_csv("C:/Users/wky0hqm/Desktop/R_scripts/forsee_data.csv")
+
+#Loading data, filtering by locale, cleaning url column, selecting only the columns that I will use
+Foresee <- read_csv("C:/Users/wky0hqm/Desktop/R_scripts/forsee_data.csv")%>% filter(locale == 'us_en') %>% 
+  separate(url, into = c("url_clean","right"),sep = "[?]") %>% 
+  select(experienceDate,oe_improvements,oe_industry,oe_navigation,oe_not_complete_task,url_clean)
 View(Foresee)
 
 
@@ -11,9 +14,23 @@ Foresee_terms <- Foresee %>% filter(locale == 'us_en') %>% filter(str_detect(oe_
                                                                            c("supply","freight","logistic","brokerage"))) %>%
   select(experienceDate,oe_improvements,oe_industry,oe_navigation,oe_not_complete_task,url)
 View(Foresee_terms) #24 rows
+write.csv(Foresee_terms,"Foresee_terms_2019.csv")
 
-#select urls linked to the experinece on UPS.com
-Foresee_url <- Foresee %>% filter(str_detect(url, c('supply','freight','logistics'))) %>% filter(locale == 'us_en') %>%
+#select urls linked to the experience on UPS.com - filtering by terms present in the urls
+Foresee_url <- Foresee %>% filter(str_detect(url, c('supply','freight','logistics', 'itl','expedited','pallet','invoice','customs','nafta'))) %>% filter(locale == 'us_en') %>%
   select(experienceDate,oe_improvements,oe_industry,oe_navigation,oe_not_complete_task,url)
 View(Foresee_url) #8 rows
+write.csv(Foresee_url,'Foresee_URL_2019.csv')
 
+#Getting the list of URLs from UPS.com that are linked to this project
+Url_list <- read_csv("C:/Users/wky0hqm/Desktop/scs_nav/list_urls_scs_ups.com_10_1_20.csv")
+View(Url_list)
+
+
+#Removing duplicates and renaming column
+Url_list <- Url_list %>% unique() %>% rename('List_of_URL' = 'https://www.ups.com/us/en/services/contract-logistics.page')
+
+
+#Filtering Foresee main table based on list of URL in the URL_list dataset
+Foresee_URL_comments <- Foresee %>% filter(url_clean %in% Url_list$List_of_URL )
+View(Foresee_URL_comments)
